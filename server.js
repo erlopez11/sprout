fe9d95b649f8a0511d421f419a10d36e6ae4bc44
erlopez11 <lopezelainy@gmail.com>
2025-03-15 15:19:55 -0500

    require morgan, methodOverride, & authController & add urlencoded middleware

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+const authController = require('./controllers/auth');
 
 const app = express();
 
 dotenv.config();
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ||'3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.status(200).render('index.ejs');
 });
 
+app.use('/auth', authController);
 
 app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`);
