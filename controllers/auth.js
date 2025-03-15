a937b0cfbf990b6d5e148d5e5a00096e334f482c
erlopez11 <lopezelainy@gmail.com>
2025-03-15 15:56:16 -0500

    wip: define route to create a new user, sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const bycrypt = require('bcrypt');
 
 const User = require('../models/user');
 
 
 router.get('/sign-up', (req, res) => {
-    res.render('auth/sign-up.ejs');
+    res.status(200).render('auth/sign-up.ejs');
 });
 
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({username: req.body.username});
+        if (userInDatabase) {
+            return res.send('Username already taken');
+        }
 
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send('Password and Confirm Password must match');
+        }
+
+        const hashedPassword = bycrypt.hashSync(req.body.password, 12);
+        req.body.password = hashedPassword;
+
+        await User.create(req.body);
+        //TODO: Redirect to the sign in page after sign up
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 
 
