2d4ea8315384c3b8d46c2e2b73ea5539316be285
erlopez11 <lopezelainy@gmail.com>
2025-03-16 18:44:51 -0500

    wip: define route to render plants index

--- controllers/plants.js
+++ controllers/plants.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-
+router.get('/', (req, res) => {
+    try {
+        const loggedInUser = User.findById(req.session.user._id);
+        res.render('plants/index.ejs', {
+            plants: loggedInUser.plants,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 
 
