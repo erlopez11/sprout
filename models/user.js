1d55f87758f30afd6ec5f686099c0e45e9ac793a
erlopez11 <lopezelainy@gmail.com>
2025-03-16 14:00:50 -0500

    wip: create plants model & embedded in user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
+const plantSchema = new mongoose.Schema({
+    plantName: {
+       type: String,
+       required: true, 
+    },
+    season: {
+        type: String,
+        required: true,
+        enum: ['summer', 'fall', 'winter', 'spring'],
+    },
+    plantType: {
+        type: String,
+        required: true,
+        enum: ['herb', 'veggie', 'fruit', 'flower', 'other']
+    },
+    quantity: {
+        type: Number,
+    },
+    notes: {
+        type: String,
+    },
+});
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -9,6 +32,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    plants: [plantSchema],
 });
 
 const User = mongoose.model('User', userSchema);
