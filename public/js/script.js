f29a06f8003ffaae974dcfa3e340e66ee260879e
erlopez11 <lopezelainy@gmail.com>
2025-03-17 17:10:18 -0500

    wip: add event listener to toggle menu for mobile

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+//Cache
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primaryNav');
+
+// Event Listeners
+
+navToggle.addEventListener('click', () => {
+    const visible = primaryNav.getAttribute('data-visible');
+
+    if (visible === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+
