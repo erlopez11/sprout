bbf5d2124ab87a31852d86e9d68a64ff9bb6ed78
erlopez11 <lopezelainy@gmail.com>
2025-03-17 12:12:23 -0500

    wip: define routes for delete, edit template, & update

--- controllers/plants.js
+++ controllers/plants.js
@@ -43,6 +43,43 @@ router.get('/:plantId', async (req, res) => {
     }
 });
 
+router.delete('/:plantId', async (req, res) => {
+    try {
+        const loggedInUser = await User.findById(req.session.user._id);
+        loggedInUser.plants.id(req.params.plantId).deleteOne();
+        await loggedInUser.save();
+        res.redirect(`/users/${loggedInUser._id}/plants`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/:plantId/edit', async (req, res) => {
+    try {
+        const loggedInUser = await User.findById(req.session.user._id);
+        const plant = loggedInUser.plants.id(req.params.plantId);
+        res.status(200).render('plants/edit.ejs', {
+            plant
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.put('/:plantId', async (req, res) => {
+    try {
+        const loggedInUser = await User.findById(req.session.user._id);
+        const plant = loggedInUser.plants.id(req.params.plantId);
+        plant.set(req.body);
+        await loggedInUser.save();
+        res.redirect(`/users/${loggedInUser._id}/plants/${req.params.plantId}`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 
 
