685a637bba32c3841e8e731a1e0d302ee6632084
erlopez11 <lopezelainy@gmail.com>
2025-03-19 15:29:38 -0500

    wip: add fade out animation to flash message

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 //Cache
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primaryNav');
+const flashMessage = document.querySelector('.flashMessage');
 
 // Event Listeners
 
@@ -16,4 +17,9 @@ navToggle.addEventListener('click', () => {
     }
 });
 
-
+if (flashMessage) {
+    setTimeout(() => {
+        flashMessage.style.transition = 'opacity 0.5s';
+        flashMessage.style.opacity = '0';
+    }, 2000)
+}
